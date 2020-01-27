07853633cc883f6bcc6ebf19b2fae1043f8c96af
Rémi Rosenthal <remi-rosenthal@hotmail.fr>
2020-01-27 17:08:40 +0100

    Initial commit

---
+++ app_final/app/src/main/java/com/example/app_final/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.app_final.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
