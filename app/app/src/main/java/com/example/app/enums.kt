d228ffc88570aa52dfded8d814253af89834caf1
Rémi Rosenthal <remi-rosenthal@hotmail.fr>
2020-02-18 15:00:48 +0100

    oubli

--- app/app/src/main/java/com/example/app/enums.kt
+++ app/app/src/main/java/com/example/app/enums.kt
@@ -1,2 +1,28 @@
 package com.example.app
 
+
+val storeChoices = listOf(
+    "-",
+    "Carrefour",
+    "Monoprix",
+    "Franprix",
+    "Leclerc",
+    "Auchan"
+)
+
+val typeChoices = mutableListOf(
+    "-",
+    "Vodka",
+    "Tequila",
+    "Rhum",
+    "Vin",
+    "Bière"
+)
+
+fun getYearChoices(): MutableList<String>
+{
+    val yearChoicesInt = (1900..2020).toMutableList()
+    val yearChoices = yearChoicesInt.map { it.toString() }.toMutableList()
+    yearChoices.add("-")
+    return yearChoices
+}
